1dc5cf7e86f8d30e910ff02354e3e80d23ff3c38
Moritz Fröhlich <Froehlich@spiegelburg.net>
2020-08-13 13:04:57 +0200

    Testing pipeline 6

--- dev/build/_test.py
+++ dev/build/_test.py
@@ -1,3 +1,11 @@
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--git-token', help='Your GitHub access token')
+parser.add_argument('--ftp-user', help='The ftp username')
+parser.add_argument('--ftp-pass', help='The ftp password')
+args = vars(parser.parse_args())
+print(args)
 
 print(os.environ)
